4da5a1ab38b9128147e7a0b77e7c1ab42b7a39f6
Gabriel, Candido <fekupe@outlook.com>
2024-10-01 14:55:27 -0300

    Tentativa de correção

--- tarefa/serializer.py
+++
@@ -1,7 +0,0 @@
-from tarefa.models import Tarefa
-from rest_framework import serializers
-
-class TarefaSerializers(serializers.ModelSerializer):
-    class Meta:
-        models = Tarefa
-        field = '__all__'
